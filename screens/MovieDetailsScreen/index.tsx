50ee8ac89f6845846a1491da3136a8352c4fd79b
Renato Ferreira de Assis <renato.assis@g.globo>
2021-04-03 17:01:07 -0300

    Add: Component EpisodeItem

--- screens/MovieDetailsScreen/index.tsx
+++ screens/MovieDetailsScreen/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+import {
+  MaterialIcons,
+  Entypo,
+  AntDesign,
+  Ionicons,
+  Feather,
+} from "@expo/vector-icons";
 import { Text, View } from "../../components/Themed";
+import { Image, Pressable } from "react-native";
 import styles from "./styles";
 import movie from "../../assets/data/movie";
-import { Image } from "react-native";
 
 const firstEpisode = movie.seasons.items[0].episodes.items[0];
 
@@ -11,6 +17,60 @@ const MovieDetailsScreen = () => {
   return (
     <View>
       <Image source={{ uri: firstEpisode.poster }} style={styles.image} />
+      <View style={{ padding: 12 }}>
+        <Text style={styles.title}>{movie.title}</Text>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={styles.match}> 98% match </Text>
+          <Text style={styles.year}>{movie.year}</Text>
+          <View style={styles.ageContainer}>
+            <Text style={styles.age}>12+</Text>
+          </View>
+          <Text style={styles.year}>{movie.numberOfSeasons} Seasons </Text>
+          <MaterialIcons name="hd" size={24} color="white" />
+        </View>
+        {/*Play Button */}
+        <Pressable
+          style={styles.playButton}
+          onPress={() => {
+            console.warn("Play");
+          }}
+        >
+          <Text style={styles.playButtonText}>
+            <Entypo name="controller-play" size={16} color="black" />
+            Play
+          </Text>
+        </Pressable>
+        {/*Download Button */}
+        <Pressable
+          style={styles.downloadButton}
+          onPress={() => {
+            console.warn("Download");
+          }}
+        >
+          <Text style={styles.downloadButtonText}>
+            <AntDesign name="download" size={16} color="white" />
+            Download
+          </Text>
+        </Pressable>
+        <Text style={{ marginVertical: 10 }}>{movie.plot}</Text>
+        <Text style={styles.year}>Cast: {movie.cast}</Text>
+        <Text style={styles.year}>Creator: {movie.creator}</Text>
+        {/*Row with icon buttons */}
+        <View style={{ flexDirection: "row", marginTop: 20 }}>
+          <View style={{ alignItems: "center", marginHorizontal: 10 }}>
+            <AntDesign name="plus" size={24} color="white" />
+            <Text style={{ color: "darkgrey", marginTop: 5 }}>My list</Text>
+          </View>
+          <View style={{ alignItems: "center", marginHorizontal: 30 }}>
+            <Feather name="thumbs-up" size={24} color="white" />
+            <Text style={{ color: "darkgrey", marginTop: 5 }}>Rate</Text>
+          </View>
+          <View style={{ alignItems: "center", marginHorizontal: 30 }}>
+            <Ionicons name="share-social" size={24} color="white" />
+            <Text style={{ color: "darkgrey", marginTop: 5 }}>Share</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
--- screens/MovieDetailsScreen/styles.ts
+++ screens/MovieDetailsScreen/styles.ts
@@ -3,7 +3,60 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   image: {
     width: "100%",
-    height: 100,
+    aspectRatio: 16 / 9,
+    resizeMode: "cover",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  match: {
+    color: "#628162",
+    fontWeight: "bold",
+    marginHorizontal: 5,
+    marginRight: 5,
+  },
+  year: {
+    color: "#757575",
+    marginRight: 5,
+  },
+  ageContainer: {
+    backgroundColor: "#e6e229",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 2,
+    paddingHorizontal: 3,
+    marginRight: 5,
+  },
+  age: {
+    color: "black",
+    fontWeight: "bold",
+  },
+  playButton: {
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 5,
+    borderRadius: 3,
+    marginVertical: 5,
+  },
+  playButtonText: {
+    color: "black",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  downloadButton: {
+    backgroundColor: "#2b2b2b",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 5,
+    borderRadius: 3,
+    marginVertical: 5,
+  },
+  downloadButtonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
 
